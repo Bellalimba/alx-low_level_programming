e7bbd69156ec37d80430f0d17ebe105729d5a644
Bellalimba <ikanirebecca7@gmail.com>
2022-09-08 18:43:58 -0700

    work

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
-#!/bin/bash
 #include <stdio.h>
 /**
- *  * main - main block
- *   * Return: 0
- *    */
+ *main - entry point
+ *Return: 0
+ */
 int main(void)
-{puts("\"Programming is like building a multilingual puzzle");return (0);}
+{printf("with proper grammar, but the outcome is a piece of art,\n");
+			return (0);
+}
