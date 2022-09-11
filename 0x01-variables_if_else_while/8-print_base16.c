9180ba5211688624107f4b2caa1cca7a0f24e208
Bellalimba <ikanirebecca7@gmail.com>
2022-09-11 14:09:20 -0700

    base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -4,20 +4,19 @@
  *   
  *Description: using the main function
  *this program prints Programming is positive, zero, or negative
- * Return: 0
+ *Return: 0
  */
 int main(void)
 {
-	char ch;
-	for (ch = 'a' ; ch <= 'z' ; ch++)
+	char c;
+	for (c = '0'; c <= '9'; c++)
 	{
-			putchar(ch);
+			putchar(c);
 	}
-	for (ch = 'A' ; ch <= 'Z' ; ch++)
+	for (c = 'a'; c <= 'f'; c++)
 	{
-			putchar(ch);
+			putchar(c);
 	}
 	putchar('\n');
 	return (0);
 }
-
