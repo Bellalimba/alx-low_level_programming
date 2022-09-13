7815f58f04424ed5d43f992b98b2bdc3f5bcdab0
Bellalimba <ikanirebecca7@gmail.com>
2022-09-13 14:31:26 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,12 +8,14 @@ int main(void)
 {
 	        int i;
 
-		        char p[] = "_putchar";
-
-			        for (i = 0; i < 8; i++)
-					        {
-							                _putchar(p[i]);
-									        }
-				        _putchar('\n');
-					        return (0);
+		char p[] = "_putchar";
+		
+		for (i = 0; i < 8; i++)
+	        {
+		      _putchar(p[i]);
+		}
+	        _putchar('\n');
+					        
+		
+	        return (0);
 }
