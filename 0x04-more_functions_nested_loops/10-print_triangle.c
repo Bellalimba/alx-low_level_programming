ad55770511e073114ce86545b92ca4f4e66ef137
Bellalimba <ikanirebecca7@gmail.com>
2022-09-15 19:00:57 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,10 +11,15 @@ void print_triangle(int size)
 {
 
 		if (size <= 0)
+
 		{
+
 			_putchar('\n');
+
 		} else
+
 		{
+
 			int i, j;
 			for (i = 1; i <= size; i++)
 			{
@@ -29,5 +34,4 @@ void print_triangle(int size)
 				_putchar('\n');
 			}
 		}
-
 }
