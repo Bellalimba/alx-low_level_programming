2acc254c534c287f44c34928f8c53ea589c8ce9e
Bellalimba <ikanirebecca7@gmail.com>
2022-09-19 15:21:09 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -13,31 +13,15 @@ int _atoi(char *s)
 {
 
 		int i, d, n, len, f, digit;
-
-
-
 		i = 0;
-
 		d = 0;
-
 		n = 0;
-
 		len = 0;
-
 		f = 0;
-
 		digit = 0;
-
-
-
 		while (s[len] != '\0')
-
 			len++;
-
-
-
 		while (i < len && f == 0)
-
 		{
 			if (s[i] == '-')
 				++d;
@@ -54,14 +38,8 @@ int _atoi(char *s)
 			}
 			i++;
 		}
-
-
-
 		if (f == 0)
 			return (0);
-
-
-
 		return (n);
 
 }
